d66a7a6f11ce40ad4a2986a73b769711dfcb18e0
Lukas Steiner <l.steiner@nexus-group.com>
2026-04-01 10:47:26 +0700

    asd

--- setup_ongedo.sh
+++ setup_ongedo.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run this script to receive $DEVOPS_PAT
-/etc/profile.d/export-pat.sh
+source /etc/profile.d/export-pat.sh
 
 DEVOPS_REMOTE="https://${DEVOPS_PAT}@dev.azure.com/NexusGroup/Ongedo/_git/Ongedo"
 REPO_DIR=$(pwd)
